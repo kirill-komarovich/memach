3ca4ea98b2650b914baffc650092832029045f5e
Kirill Komarovich <still2405@gmail.com>
2019-08-23 11:55:12 +0300

    Rename Application to Bot (#3)

--- Rakefile
+++ Rakefile
@@ -4,12 +4,12 @@ require 'bundler/setup'
 require_relative 'system/container'
 
 STDOUT.sync = true
-Bundler.require(:default, Application.env)
-Dotenv.load('.env', ".env.#{Application.env}")
+Bundler.require(:default, Bot.env)
+Dotenv.load('.env', ".env.#{Bot.env}")
 
 Dir.glob('lib/tasks/*.rake').each { |r| load r }
 
-if %i[development test].include? Application.env
+if %i[development test].include? Bot.env
   require 'rubocop/rake_task'
   require 'bundler/audit/task'
   require 'rspec/core/rake_task'
--- system/boot/database.rb
+++ system/boot/database.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-Application.boot(:database) do
+Bot.boot(:database) do
   init do
     require 'active_record'
     require 'pg'
   end
 
   start do
-    ActiveRecord::Base.logger = Application.logger unless Application.env == :test
-    ActiveRecord::Base.establish_connection(Application.database_config.fetch(Application.env.to_s))
+    ActiveRecord::Base.logger = Bot.logger unless Bot.env == :test
+    ActiveRecord::Base.establish_connection(Bot.database_config.fetch(Bot.env.to_s))
   end
 end
