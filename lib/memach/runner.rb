7e8268c3a5235fe63064306b3c34252c986c563f
Kirill Komarovich <still2405@gmail.com>
2019-08-23 11:45:13 +0300

    Correct rubocop offences

--- lib/memach/runner.rb
+++ lib/memach/runner.rb
@@ -11,8 +11,6 @@ module Memach
         case message.text
         when '/start'
           bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-        when '/stop'
-          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
         end
       end
     end
