527c38a918313fc2c8d4034c57681ddfb9f6fe66
Kirill Komarovich <still2405@gmail.com>
2019-11-21 20:43:27 +0300

    Move transactions to operation level (#17)

--- lib/repositories/user_repository.rb
+++ lib/repositories/user_repository.rb
@@ -7,11 +7,5 @@ module Repositories
     include Import.args[:rom]
 
     commands :create, mapper: :user
-
-    def create(attributes)
-      users.transaction do
-        users.command(:create).call(attributes)
-      end
-    end
   end
 end
