84d7fc66d02c791dba79a8f25f3ea16021df24e2
Kirill Komarovich <still2405@gmail.com>
2019-11-28 20:27:32 +0300

    Move CommandHandler to module (#21)

--- lib/handlers/command_handler.rb
+++ lib/handlers/command_handler.rb
@@ -1,53 +1,65 @@
 # frozen_string_literal: true
 
 module Handlers
-  class CommandHandler < ApplicationHandler
-    class << self
-      attr_reader :command
+  class CommandHandler < Module
+    ClassMethods = Class.new(Module)
+    InstanceMethods = Class.new(Module)
 
+    class << self
       def command_handlers
         @command_handlers ||= []
-      end
-
-      def inherited(subclass)
-        command_handlers << subclass
-      end
-
-      def for_command(command)
-        @command = command
-      end
-
-      def alias_commands(*command_aliases)
-        @command_aliases = command_aliases
-      end
-
-      def command_aliases
-        @command_aliases ||= []
-      end
-
-      def can_handle?(message)
-        message.start_with?(command, *command_aliases)
       end
 
       def handler_class(message)
         command_handlers.detect { |handler| handler.can_handle?(message) }
       end
+
+      def [](*commands)
+        new(commands)
+      end
     end
 
-    def command
-      self.class.command
+    attr_reader :commands, :class_mod, :instance_mod
+
+    def initialize(commands)
+      @commands = commands
+      @class_mod = ClassMethods.new
+      @instance_mod = InstanceMethods.new
     end
 
-    def command_aliases
-      self.class.command_aliases
+    def included(klass)
+      define_can_handle
+      define_instance_methods
+
+      klass.send(:include, instance_mod)
+      klass.extend(class_mod)
+
+      self.class.command_handlers << klass
+
+      super
     end
 
-    def command_arguments
-      @command_arguments ||= message.text.gsub(/^(#{command_filter})(\s+|$)/, '').split
+    def define_can_handle
+      class_mod.class_exec(commands) do |commands|
+        define_method :can_handle? do |message|
+          message.start_with?(*commands)
+        end
+      end
     end
 
-    def command_filter
-      (command_aliases << command).join('|')
+    def define_instance_methods
+      instance_mod.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+        attr_reader :commands
+
+        def initialize(params)
+          @commands = #{commands}
+          super(params)
+        end
+
+        def command_arguments
+          @command_arguments ||= current_message.text.gsub(/^(\#{commands.join('|')})(\s+|$)/, '').split
+        end
+      RUBY
     end
   end
 end
